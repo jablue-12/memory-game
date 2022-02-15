7f393c7a2dd7734389ced904173f7e526bd86706
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2022-02-14 20:38:52 -0600

    set up matrix and displaying the cards.
    - Created a matrix that accepts a pre defined dimension (should be even number).
    - Also created a card component that displays the content of the card.
    - Started the implementation of the game functionality of a card. i.e. clicking a card will 'flip' the content.

---
+++ src/components/Card.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="m-2">
+        <b-button class="cardSize" @click="cardClicked()">
+            <h3>{{item.id}}</h3>
+            <p v-if="isFrontSide">{{item.name}}</p>
+        </b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    },//end props
+
+    data(){
+        return {
+            isFrontSide: false,
+            twoChosenCards: [],
+        }
+    },//end data
+
+    methods:{
+        cardClicked(){
+            //only flip when a card has no pair yet
+            if(this.twoChosenCards.length >= 0 && this.twoChosenCards.length <=2){
+
+                if(!this.item.hasPair){
+                    // console.log("clicked card with id " + this.item.id);
+                    this.isFrontSide = !this.isFrontSide;
+                    this.$emit("card-clicked",this.item);
+                }//end nested if
+               
+            }//end if
+           
+        },//end cardClicked
+
+    },//end methods
+}
+</script>
+
+<style scoped>
+    .cardSize {
+        width: 110px;
+        height: 110px;
+        /* min-width: fit-content;
+        min-height: fit-content; */
+    }
+
+</style>
