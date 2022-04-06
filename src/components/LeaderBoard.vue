05f64f79fbdf79bfadad7e4bb8b23e325dd481d8
jablue-12 <jaredcastillo551@gmail.com>
2022-04-05 23:42:10 -0500

    user-scoreboard ============== included search,select and pagination on the leaderboard page. Still in progress for implementation.

--- src/components/LeaderBoard.vue
+++ src/components/LeaderBoard.vue
@@ -1,18 +1,40 @@
 <template>
     <b-container fluid>
-        <b-table
-            class="mt-2"
-            :items="allUsers"
-            striped
-            responsive
-            outlined
-            >
 
-            <template v-slot:cell(username)="row">
-                <strong>{{row.item.username === currentUser.username ? '(You) ' : ''}}</strong>{{row.item.username}}
-            </template>
+        <b-row class="mt-2">
+            <b-col cols="4"> 
+                <b-form-select
+                    v-model="selected">
+                    <template #first>
+                        <b-form-select-option :value="null" disabled>-- Please select an option --</b-form-select-option>
+                    </template>
+                </b-form-select>
+            </b-col>
+            <b-col md="3" class="ml-md-auto"><b-input placeholder="Search..."></b-input></b-col>
+        </b-row>
 
-        </b-table>
+        <b-row>
+            <b-col>
+                <b-table
+                    class="mt-2"
+                    :items="allUsers"
+                    striped
+                    responsive
+                    outlined
+                    >
+
+                    <template v-slot:cell(username)="row">
+                        <strong>{{row.item.username === currentUser.username ? '(You) ' : ''}}</strong>{{row.item.username}}
+                    </template>
+
+                </b-table>
+
+           </b-col>
+        </b-row>
+
+        
+        <b-pagination align="right"></b-pagination>
+
     </b-container>
 </template>
 
@@ -20,6 +42,13 @@
 import {mapGetters,mapActions} from "vuex";
 export default {
     name: "LeaderBoard",
+
+    data(){
+        return {
+            //TODO: search/select filter and pagination data
+            selected: null,
+        }
+    },
 
     created(){
         this.current();
